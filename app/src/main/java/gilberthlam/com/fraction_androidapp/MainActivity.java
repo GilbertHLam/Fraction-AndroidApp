665fc64389004c0be4b0c2f5a9ba7807ca2dea98
GilbertHLam <gilberthlam@cmail.carleton.ca>
2016-11-28 13:55:51 -0500

    Removed the title bar and added icons for the app.

--- app/src/main/java/gilberthlam/com/fraction_androidapp/MainActivity.java
+++ app/src/main/java/gilberthlam/com/fraction_androidapp/MainActivity.java
@@ -3,6 +3,7 @@ package gilberthlam.com.fraction_androidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final DecimalFormat df = new DecimalFormat("###.#");
