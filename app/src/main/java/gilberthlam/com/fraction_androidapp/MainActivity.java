553ecd1660e338bbf3252966a164d854856b2f9b
GilbertHLam <gilberthlam@cmail.carleton.ca>
2016-11-28 14:11:42 -0500

    Fixed a bug where app would crash when the negative was entered after a number

--- app/src/main/java/gilberthlam/com/fraction_androidapp/MainActivity.java
+++ app/src/main/java/gilberthlam/com/fraction_androidapp/MainActivity.java
@@ -29,66 +29,69 @@ public class MainActivity extends AppCompatActivity {
                 boolean pos = true;
                 TextView status = (TextView) findViewById(R.id.statusLabel);
                 double comFac;
-                double numer = Double.parseDouble(numeratorBox.getText().toString());
-                double denom = Double.parseDouble(denominatorBox.getText().toString());
-                double startnumer = numer;
-                double startdenom = denom;
-                if (denom == 0) {
-                    status.setText("INVALID FRACTION");
+                try {
+                    double numer = Double.parseDouble(numeratorBox.getText().toString());
+                    double denom = Double.parseDouble(denominatorBox.getText().toString());
+                    double startnumer = numer;
+                    double startdenom = denom;
+                    if (denom == 0) {
+                        status.setText("INVALID FRACTION");
+                    } else {
+                        if (status.getText().toString().equals("CALCULATING...")) {
+
+                        } else {
+                            status.setText("CALCULATING...");
+                            if (denom < 0 && numer > 0) {
+                                denom = denom * -1;
+                                pos = false;
+                            } else if (numer < 0 && denom > 0) {
+                                numer = numer * -1;
+                                pos = false;
+                            } else if (numer < 0 && denom < 0) {
+                                denom = denom * -1;
+                                numer = numer * -1;
+                            }
+
+                            if (denom > numer) {
+                                for (double i = numer; i > 0; i--) {
+                                    if (numer % i == 0 && denom % i == 0) {
+                                        numer = numer / i;
+                                        denom = denom / i;
+                                        comFac = i;
+                                        break;
+                                    }
+                                }
+                            }
+                            if (numer > denom) {
+                                for (double i = denom; i > 0; i--) {
+                                    if (numer % i == 0 && denom % i == 0) {
+                                        numer = numer / i;
+                                        denom = denom / i;
+                                        comFac = i;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if (pos == false) {
+                                numeratorBox.setText("-" + df.format(numer));
+                                denominatorBox.setText("" + df.format(denom));
+                            } else {
+                                numeratorBox.setText("" + (df.format(numer)));
+                                denominatorBox.setText("" + df.format(denom));
+                            }
+                            if (startdenom == denom && startnumer == numer) {
+                                status.setText("ALREADY SIMPLIFIED");
+                            } else {
+                                status.setText("FRACTION SIMPLIFIED!");
+                            }
+
+                        }
+
+                    }
                 }
-                else {
-                    if (status.getText().toString().equals("CALCULATING...")) {
-
-                    }
-                    else {
-                        status.setText("CALCULATING...");
-                        if (denom < 0 && numer > 0) {
-                            denom = denom * -1;
-                            pos = false;
-                        } else if (numer < 0 && denom > 0) {
-                            numer = numer * -1;
-                            pos = false;
-                        } else if (numer < 0 && denom < 0) {
-                            denom = denom * -1;
-                            numer = numer * -1;
-                        }
-
-                        if (denom > numer) {
-                            for (double i = numer; i > 0; i--) {
-                                if (numer % i == 0 && denom % i == 0) {
-                                    numer = numer / i;
-                                    denom = denom / i;
-                                    comFac = i;
-                                    break;
-                                }
-                            }
-                        }
-                        if (numer > denom) {
-                            for (double i = denom; i > 0; i--) {
-                                if (numer % i == 0 && denom % i == 0) {
-                                    numer = numer / i;
-                                    denom = denom / i;
-                                    comFac = i;
-                                    break;
-                                }
-                            }
-                        }
-
-                        if (pos == false) {
-                            numeratorBox.setText("-" + df.format(numer));
-                            denominatorBox.setText("" + df.format(denom));
-                        } else {
-                            numeratorBox.setText("" + (df.format(numer)));
-                            denominatorBox.setText("" + df.format(denom));
-                        }
-                        if (startdenom == denom && startnumer == numer) {
-                            status.setText("ALREADY SIMPLIFIED");
-                        } else {
-                            status.setText("FRACTION SIMPLIFIED!");
-                        }
-
-                    }
-
+                catch (NumberFormatException e){
+                    status.setText("INVALID FRACTION!");
                 }
             }
         });
